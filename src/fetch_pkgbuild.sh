e8e33f5c6cf39b7175f04225c475ab4760c2b012
Yifan Zhu <fanzhuyifan@gmail.com>
2023-12-02 12:15:36 -0800

    dolphin patch

--- src/fetch_pkgbuild.sh
+++ src/fetch_pkgbuild.sh
@@ -6,11 +6,29 @@
 # git repository. It will use the latest tagged version of the PKGBUILD
 
 BRANCH="makepkg"
+
 pkgctl repo clone --protocol https "$1"
 cd "$1"
 REMOTE_BRANCH=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
-echo "Fetching PKGBUILD from $REMOTE_BRANCH"
-git reset --hard; git checkout "$REMOTE_BRANCH"; git pull
+
+git reset --hard
+git checkout "$REMOTE_BRANCH"
+git pull
 git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
-git branch -D "$BRANCH" ; git checkout -b "$BRANCH"
+git branch -D "$BRANCH"
+git checkout -b "$BRANCH"
+
+custom_patch() {
+    if [ "$1" = "dolphin" ]; then
+        # wget 'https://gitlab.archlinux.org/archlinux/packaging/packages/dolphin/-/raw/kde-unstable/plasma-activities.patch' -O plasma-activities.patch
+        cat <<EOF >>PKGBUILD
+prepare() {
+    :
+}
+EOF
+    fi
+}
+
+custom_patch "$1"
+
 cd ..
