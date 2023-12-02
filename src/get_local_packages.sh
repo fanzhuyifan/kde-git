05aa9e1ba22038864288c0826085f80657dc2ded
Yifan Zhu <fanzhuyifan@gmail.com>
2023-12-02 14:30:52 -0800

    pkglist

--- src/get_local_packages.sh
+++ src/get_local_packages.sh
@@ -2,4 +2,5 @@
 
 # Prints all installed KDE packages
 
-comm -12 <(pacman -Qq | sort) <(get_pkglist.sh 2>/dev/null | sort)
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+comm -12 <(pacman -Qq | sort) "$DIR"/pkglist.txt
